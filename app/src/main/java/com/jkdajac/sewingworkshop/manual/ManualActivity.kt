a20d338117416357dc7893e34e4811d59ca11c20
ARTEM <jkda@ukr.net>
2021-09-07 18:11:52 +0300

    added Manual Activity

---
+++ app/src/main/java/com/jkdajac/sewingworkshop/manual/ManualActivity.kt
@@ -0,0 +1,12 @@
+package com.jkdajac.sewingworkshop.manual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jkdajac.sewingworkshop.R
+
+class ManualActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual)
+    }
+}
