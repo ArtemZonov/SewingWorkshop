c8c2911a5c01eea89a33cf74a70ed503477dda52
ARTEM <jkda@ukr.net>
2021-09-09 23:22:46 +0300

    added Formuli Activity

--- app/src/main/java/com/jkdajac/sewingworkshop/manual/FormuliActivity.kt
+++ app/src/main/java/com/jkdajac/sewingworkshop/manual/FormuliActivity.kt
@@ -2,6 +2,7 @@ package com.jkdajac.sewingworkshop.manual
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jkdajac.sewingworkshop.R
 
 class FormuliActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
